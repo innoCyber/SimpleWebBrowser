0b75cf742e74aeda016000c8a1e4afb3ee355071
innoCyber <innocentileka@gmail.com>
2018-08-16 05:59:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/innocyber/simplewebbrowser/myWebViewClient.java
@@ -0,0 +1,16 @@
+package com.example.innocyber.simplewebbrowser;
+
+import android.webkit.CookieManager;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+class myWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        CookieManager.getInstance().setAcceptCookie(true);
+        return true;
+    }
+}
